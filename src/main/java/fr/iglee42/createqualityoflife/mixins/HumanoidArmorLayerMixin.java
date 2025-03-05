e5b752b13af670b82059445798d6a8548af9c2a9
iglée42 <igleev2@gmail.com>
2025-03-05 12:24:50 +0100

    Fix mixins

--- src/main/java/fr/iglee42/createqualityoflife/mixins/HumanoidArmorLayerMixin.java
+++ src/main/java/fr/iglee42/createqualityoflife/mixins/HumanoidArmorLayerMixin.java
@@ -17,7 +17,7 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 @Mixin(HumanoidArmorLayer.class)
 public class HumanoidArmorLayerMixin<T extends LivingEntity, M extends HumanoidModel<T>, A extends HumanoidModel<T>> {
 
-    @Inject(remap = false, method = "renderArmorPiece",at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;getItem()Lnet/minecraft/world/item/Item;",shift = At.Shift.BEFORE),cancellable = true,locals = LocalCapture.CAPTURE_FAILSOFT)
+    @Inject( method = "renderArmorPiece",at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;getItem()Lnet/minecraft/world/item/Item;",shift = At.Shift.BEFORE),cancellable = true,locals = LocalCapture.CAPTURE_FAILSOFT)
     private void createqol$hideArmor(PoseStack p_117119_, MultiBufferSource p_117120_, T p_117121_, EquipmentSlot p_117122_, int p_117123_, A p_117124_, CallbackInfo ci, ItemStack itemstack){
         if (!NBTConstants.getOrDefault(itemstack,NBTConstants.NBT_RENDER_TYPE).shouldRenderArmor()) ci.cancel();
     }
--- src/main/java/fr/iglee42/createqualityoflife/mixins/LavaSwimmingMixin.java
+++ src/main/java/fr/iglee42/createqualityoflife/mixins/LavaSwimmingMixin.java
@@ -22,7 +22,7 @@ public abstract class LavaSwimmingMixin extends Entity {
 		super(type, level);
 	}
 
-	@Inject(remap = false,method = "travel(Lnet/minecraft/world/phys/Vec3;)V", slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;isInLava()Z")), at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;move(Lnet/minecraft/world/entity/MoverType;Lnet/minecraft/world/phys/Vec3;)V", shift = Shift.AFTER, ordinal = 0))
+	@Inject(method = "travel(Lnet/minecraft/world/phys/Vec3;)V", slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;isInLava()Z")), at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;move(Lnet/minecraft/world/entity/MoverType;Lnet/minecraft/world/phys/Vec3;)V", shift = Shift.AFTER, ordinal = 0))
 	private void create$onLavaTravel(Vec3 travelVector, CallbackInfo ci) {
 		ItemStack bootsStack = DivingBootsItem.getWornItem(this);
 		if (ModItems.SHADOW_RADIANCE_BOOTS.isIn(bootsStack) && NBTConstants.getOrDefault(bootsStack,NBTConstants.NBT_LAVA,true))
