17918ca514ae20e9ad213b37e8c5776c4c9e21d5
iglée42 <igleev2@gmail.com>
2025-03-19 13:30:19 +0100

    Fix #16

--- src/main/java/fr/iglee42/createqualityoflife/items/ShadowRadianceChestplate.java
+++ src/main/java/fr/iglee42/createqualityoflife/items/ShadowRadianceChestplate.java
@@ -43,7 +43,7 @@ public class ShadowRadianceChestplate extends BacktankItem.Layered{
             if (BacktankUtil.getAllWithAir(player).isEmpty()) return;
             if (stack.getOrDefault(ModDataComponents.ARMOR_EFFECT,true))
                 player.addEffect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, 20, 1, false, false));
-            if (player.isCreative()) return;
+            if (player.isCreative() || player.isSpectator()) return;
             if (isFansEnable(stack) && !BacktankUtil.getAllWithAir(player).isEmpty() && hasPropeller(stack)) {
                 boolean hover = isHoverEnable(stack);
                 boolean jumpKeyActive = CommonKeysHandler.isHoldingUp(player);
