69aac1fcb6ac1eec59fad00f7fb4dffa0b5bf560
iglée42 <igleev2@gmail.com>
2025-08-04 23:22:58 +0200

    Data Gen
    # Conflicts:
    #	src/generated/resources/data/create/tags/item/contraption_controlled.json
    #	src/generated/resources/data/minecraft/tags/block/mineable/axe.json
    #	src/main/java/fr/iglee42/createqualityoflife/registries/QOLBlocks.java
    #	src/main/java/fr/iglee42/createqualityoflife/registries/QOLItems.java
    #	src/main/resources/data/minecraft/tags/block/mineable/axe.json
    #	src/main/resources/data/minecraft/tags/block/mineable/pickaxe.json
    #	src/main/resources/data/minecraft/tags/blocks/mineable/axe.json
    #	src/main/resources/data/minecraft/tags/blocks/mineable/pickaxe.json

--- src/main/java/fr/iglee42/createqualityoflife/registries/QOLItems.java
+++ src/main/java/fr/iglee42/createqualityoflife/registries/QOLItems.java
@@ -3,12 +3,12 @@ package fr.iglee42.createqualityoflife.registries;
 import com.simibubi.create.AllTags;
 import com.simibubi.create.content.equipment.armor.BacktankItem;
 import com.simibubi.create.content.equipment.armor.BaseArmorItem;
-import com.simibubi.create.content.equipment.tool.AllToolMaterials;
 import com.simibubi.create.content.legacy.NoGravMagicalDohickyItem;
 import com.simibubi.create.content.logistics.box.PackageItem;
 import com.simibubi.create.content.logistics.box.PackageStyles;
 import com.simibubi.create.foundation.data.AssetLookup;
 import com.simibubi.create.foundation.item.ItemDescription;
+import com.tterrag.registrate.providers.ProviderType;
 import com.tterrag.registrate.util.entry.ItemEntry;
 import fr.iglee42.createqualityoflife.CreateQOL;
 import fr.iglee42.createqualityoflife.items.*;
@@ -16,7 +16,10 @@ import fr.iglee42.createqualityoflife.items.armors.*;
 import fr.iglee42.createqualityoflife.items.tools.shadowsteel.ShadowSteelPickaxe;
 import fr.iglee42.createqualityoflife.statue.StatueItem;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.item.*;
+import net.neoforged.neoforge.client.model.generators.ModelFile;
+import net.neoforged.neoforge.common.Tags;
 
 import static com.simibubi.create.AllTags.forgeItemTag;
 import static fr.iglee42.createqualityoflife.CreateQOL.REGISTRATE;
@@ -29,6 +32,9 @@ public class QOLItems {
 
     public static ItemEntry<PlayerPaperItem> PLAYER_PAPER = REGISTRATE.item("player_paper", PlayerPaperItem::new)
             .properties(properties -> properties.stacksTo(1).rarity(Rarity.RARE))
+            .model((c,p)->p.generated(c,CreateQOL.asResource("item/"+c.getName()+"_empty")).override().predicate(CreateQOL.asResource("has_player"),1f)
+                    .model(p.getBuilder(p.name(c)+"_full").parent(new ModelFile.UncheckedModelFile("item/generated")).texture("layer0",CreateQOL.asResource("item/"+c.getName()+"_full")))
+                    .end())
             .register();
 
     public static final ItemEntry<NoGravMagicalDohickyItem> SHADOW_RADIANCE = REGISTRATE.item("shadow_radiance", NoGravMagicalDohickyItem::new)
@@ -42,6 +48,7 @@ public class QOLItems {
                     ShadowRadianceHelmet::new)
             .properties(p -> p.fireResistant().rarity(Rarity.EPIC))
             .tag(forgeItemTag("armors/helmets"))
+            .tag(ItemTags.HEAD_ARMOR)
             .onRegisterAfter(Registries.ITEM, it-> ItemDescription.useKey(it,"item.createqol.shadow_armor"))
             .register();
 
@@ -54,10 +61,13 @@ public class QOLItems {
             .item("shadow_radiance_chestplate",
                     p -> new ShadowRadianceChestplate(p,
                             SHADOW_RADIANCE_CHESTPLATE_PLACEABLE))
-            .model(AssetLookup.customGenericItemModel("_", "item"))
+            .model((c,p)->p.generated(c).override().predicate(CreateQOL.asResource("elytra"),1)
+                    .model(p.getBuilder(p.name(c)+"_elytra").parent(new ModelFile.UncheckedModelFile("item/generated")).texture("layer0",CreateQOL.asResource("item/"+c.getName()+"_elytra")))
+                    .end())
             .properties(p -> p.fireResistant().rarity(Rarity.EPIC))
             .tag(AllTags.AllItemTags.PRESSURIZED_AIR_SOURCES.tag)
             .tag(forgeItemTag("armors/chestplates"))
+            .tag(ItemTags.CHEST_ARMOR)
             .register();
 
     public static final ItemEntry<? extends BaseArmorItem> SHADOW_RADIANCE_LEGGINGS = REGISTRATE
@@ -65,6 +75,7 @@ public class QOLItems {
                     p -> new ShadowRadianceArmorItem(ArmorItem.Type.LEGGINGS, p))
             .properties(p -> p.fireResistant().rarity(Rarity.EPIC))
             .tag(forgeItemTag("armors/leggings"))
+            .tag(ItemTags.LEG_ARMOR)
             .onRegisterAfter(Registries.ITEM, it-> ItemDescription.useKey(it,"item.createqol.shadow_armor"))
             .register();
 
@@ -73,6 +84,7 @@ public class QOLItems {
                     p -> new ShadowRadianceArmorItem(ArmorItem.Type.BOOTS, p))
             .properties(p -> p.fireResistant().rarity(Rarity.EPIC))
             .tag(forgeItemTag("armors/boots"))
+            .tag(ItemTags.FOOT_ARMOR)
             .onRegisterAfter(Registries.ITEM, it-> ItemDescription.useKey(it,"item.createqol.shadow_armor"))
             .register();
 
@@ -86,6 +98,7 @@ public class QOLItems {
                     RefinedRadianceHelmet::new)
             .properties(p -> p.fireResistant().rarity(Rarity.RARE))
             .tag(forgeItemTag("armors/helmets"))
+            .tag(ItemTags.HEAD_ARMOR)
             .onRegisterAfter(Registries.ITEM, it-> ItemDescription.useKey(it,"item.createqol.shadow_armor"))
             .register();
     public static final ItemEntry<BacktankItem.BacktankBlockItem> REFINED_RADIANCE_CHESTPLATE_PLACEABLE = REGISTRATE
@@ -97,17 +110,21 @@ public class QOLItems {
             .item("refined_radiance_chestplate",
                     p -> new RefinedRadianceChestplate(p,
                     REFINED_RADIANCE_CHESTPLATE_PLACEABLE))
-            .model(AssetLookup.customGenericItemModel("_", "item"))
+            .model((c,p)->p.generated(c).override().predicate(CreateQOL.asResource("elytra"),1)
+                    .model(p.getBuilder(p.name(c)+"_elytra").parent(new ModelFile.UncheckedModelFile("item/generated")).texture("layer0",CreateQOL.asResource("item/"+c.getName()+"_elytra")))
+                    .end())
             .properties(p -> p.fireResistant().rarity(Rarity.RARE))
             .tag(AllTags.AllItemTags.PRESSURIZED_AIR_SOURCES.tag)
             .tag(forgeItemTag("armors/chestplates"))
+            .tag(ItemTags.CHEST_ARMOR)
             .register();
 
     public static final ItemEntry<? extends BaseArmorItem> REFINED_RADIANCE_LEGGINGS = REGISTRATE
             .item("refined_radiance_leggings",
                     p -> new RefinedRadianceArmorItem(ArmorItem.Type.LEGGINGS, p))
             .properties(p -> p.fireResistant().rarity(Rarity.RARE).durability(1110).component(QOLDataComponents.ARMOR_RENDER_TYPE, ArmorRenderType.ALL))
-            .tag(commonItemTag("armors/leggings"))
+            .tag(forgeItemTag("armors/leggings"))
+            .tag(ItemTags.LEG_ARMOR)
             .onRegisterAfter(Registries.ITEM, it-> ItemDescription.useKey(it,"item.createqol.shadow_armor"))
             .register();
 
@@ -115,7 +132,8 @@ public class QOLItems {
             .item("refined_radiance_boots",
                     p -> new RefinedRadianceArmorItem(ArmorItem.Type.BOOTS, p))
             .properties(p -> p.fireResistant().rarity(Rarity.RARE).durability(962).component(QOLDataComponents.ARMOR_RENDER_TYPE, ArmorRenderType.ALL))
-            .tag(commonItemTag("armors/boots"))
+            .tag(forgeItemTag("armors/boots"))
+            .tag(ItemTags.FOOT_ARMOR)
             .onRegisterAfter(Registries.ITEM, it-> ItemDescription.useKey(it,"item.createqol.shadow_armor"))
             .register();
 
@@ -126,6 +144,7 @@ public class QOLItems {
                     ShadowSteelHelmet::new)
             .properties(p -> p.fireResistant().rarity(Rarity.RARE))
             .tag(forgeItemTag("armors/helmets"))
+            .tag(ItemTags.HEAD_ARMOR)
             .onRegisterAfter(Registries.ITEM, it-> ItemDescription.useKey(it,"item.createqol.shadow_armor"))
             .register();
     public static final ItemEntry<BacktankItem.BacktankBlockItem> SHADOW_STEEL_CHESTPLATE_PLACEABLE = REGISTRATE
@@ -137,17 +156,18 @@ public class QOLItems {
             .item("shadow_steel_chestplate",
                     p -> new ShadowSteelChestplate(p,
                             SHADOW_STEEL_CHESTPLATE_PLACEABLE))
-            .model(AssetLookup.customGenericItemModel("_", "item"))
             .properties(p -> p.fireResistant().rarity(Rarity.RARE))
             .tag(AllTags.AllItemTags.PRESSURIZED_AIR_SOURCES.tag)
             .tag(forgeItemTag("armors/chestplates"))
+            .tag(ItemTags.CHEST_ARMOR)
             .register();
 
     public static final ItemEntry<? extends BaseArmorItem> SHADOW_STEEL_LEGGINGS = REGISTRATE
             .item("shadow_steel_leggings",
                     p -> new ShadowSteelArmorItem(ArmorItem.Type.LEGGINGS, p))
             .properties(p -> p.fireResistant().rarity(Rarity.RARE).durability(1110).component(QOLDataComponents.ARMOR_RENDER_TYPE, ArmorRenderType.ALL))
-            .tag(commonItemTag("armors/leggings"))
+            .tag(forgeItemTag("armors/leggings"))
+            .tag(ItemTags.LEG_ARMOR)
             .onRegisterAfter(Registries.ITEM, it-> ItemDescription.useKey(it,"item.createqol.shadow_armor"))
             .register();
 
@@ -155,7 +175,8 @@ public class QOLItems {
             .item("shadow_steel_boots",
                     p -> new ShadowSteelArmorItem(ArmorItem.Type.BOOTS, p))
             .properties(p -> p.fireResistant().rarity(Rarity.RARE).durability(962).component(QOLDataComponents.ARMOR_RENDER_TYPE, ArmorRenderType.ALL))
-            .tag(commonItemTag("armors/boots"))
+            .tag(forgeItemTag("armors/boots"))
+            .tag(ItemTags.FOOT_ARMOR)
             .onRegisterAfter(Registries.ITEM, it-> ItemDescription.useKey(it,"item.createqol.shadow_armor"))
             .register();
 
@@ -167,7 +188,9 @@ public class QOLItems {
             .properties(Item.Properties::fireResistant)
             .properties(p -> p.attributes(PickaxeItem
                     .createAttributes(QOLTiers.SHADOW_STEEL, 1, -2.8f)))
-            .model(AssetLookup.itemModelWithPartials())
+            .model((c,p)->p.handheld(c))
+            .tag(ItemTags.PICKAXES)
+            .tag(ItemTags.CLUSTER_MAX_HARVESTABLES)
             .register();
 
     public static final PackageStyles.PackageStyle IGLEE = new PackageStyles.PackageStyle("rare_iglee", 12, 10, 21f, true);
@@ -183,6 +206,10 @@ public class QOLItems {
     private static ItemEntry<PackageItem> createRarePackage(PackageStyles.PackageStyle style){
         return REGISTRATE.item(style.getItemId()
                         .getPath(), p -> new PackageItem(p, style))
-                .properties(p -> p.stacksTo(1)).register();
+                .properties(p -> p.stacksTo(1))
+                .model(AssetLookup.existingItemModel())
+                .tag(AllTags.AllItemTags.PACKAGES.tag)
+                .setData(ProviderType.LANG,(c,p)->{})
+                .register();
     }
 }
