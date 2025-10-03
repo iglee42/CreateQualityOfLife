626edbf229f4b2afa7370b597a2f1ea69c73a278
Daniel Kirschbaum <info@dakiba.eu>
2025-10-03 21:04:02 +0200

    Added toggle between fans and elytra. Implement #45

---
+++ src/main/java/fr/iglee42/createqualityoflife/packets/ToggleFansElytraPacket.java
@@ -0,0 +1,42 @@
+package fr.iglee42.createqualityoflife.packets;
+
+import com.simibubi.create.content.equipment.armor.BacktankItem;
+import fr.iglee42.createqualityoflife.CreateQOLLang;
+import fr.iglee42.createqualityoflife.items.armors.ShadowRadianceChestplate;
+import fr.iglee42.createqualityoflife.registries.QOLItems;
+import fr.iglee42.createqualityoflife.registries.QOLPackets;
+import net.createmod.catnip.net.base.ServerboundPacketPayload;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+
+public class ToggleFansElytraPacket implements ServerboundPacketPayload {
+
+    public static final ToggleFansElytraPacket INSTANCE = new ToggleFansElytraPacket();
+    public static final StreamCodec<FriendlyByteBuf,ToggleFansElytraPacket> STREAM_CODEC = StreamCodec.unit(INSTANCE);
+
+
+    @Override
+    public void handle(ServerPlayer player) {
+            if (player != null) {
+                ItemStack chestplate = player.getItemBySlot(EquipmentSlot.CHEST);
+                Item backtank = BacktankItem.getWornBy(player);
+
+                if (backtank == null) return;
+                if (QOLItems.SHADOW_RADIANCE_CHESTPLATE.is(backtank) && (ShadowRadianceChestplate.hasPropeller(chestplate) || ShadowRadianceChestplate.hasElytra(chestplate))) {
+                    ShadowRadianceChestplate.toggleFansElytra(chestplate,player);
+                }else if (QOLItems.SHADOW_RADIANCE_CHESTPLATE.is(backtank) && !ShadowRadianceChestplate.hasPropeller(chestplate)){
+                    player.sendSystemMessage(CreateQOLLang.translateDirect("armor.ability.no_elytra_or_propeller").withStyle(ChatFormatting.RED),true);
+                }
+            }
+    }
+
+    @Override
+    public PacketTypeProvider getTypeProvider() {
+        return QOLPackets.TOGGLE_FANS_ELYTRA;
+    }
+}
