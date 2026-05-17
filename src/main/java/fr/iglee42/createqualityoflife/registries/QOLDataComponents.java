fbf09f8ecf4188e4aeedffd666b97f8dba7e7ddc
Gytube <fdelattre12@gmail.com>
2026-05-17 13:42:13 +0200

    small fixes

--- src/main/java/fr/iglee42/createqualityoflife/registries/QOLDataComponents.java
+++ src/main/java/fr/iglee42/createqualityoflife/registries/QOLDataComponents.java
@@ -51,7 +51,6 @@ public class QOLDataComponents {
     public static final DataComponentType<ShadowRadianceEffects> EFFECT = register("preferred_effect", b->b.persistent(ShadowRadianceEffects.CODEC).networkSynchronized(ShadowRadianceEffects.STREAM_CODEC));
     public static final DataComponentType<CompoundTag> COPIED_DATAS = register("copied_datas",b->b.persistent(CompoundTag.CODEC).networkSynchronized(ByteBufCodecs.COMPOUND_TAG));
     public static final DataComponentType<ItemTooltips> ITEM_TOOLTIPS = register("item_tooltips", b->b.persistent(ItemTooltips.CODEC).networkSynchronized(ItemTooltips.STREAM_CODEC));
-
     public static final DataComponentType<Boolean> USE_AIR = register("use_air",b->b.persistent(Codec.BOOL).networkSynchronized(ByteBufCodecs.BOOL));
 
     private static <T> DataComponentType<T> register(String name, UnaryOperator<DataComponentType.Builder<T>> builder) {
