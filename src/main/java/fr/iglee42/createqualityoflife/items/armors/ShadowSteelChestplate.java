551d91e453cbd2f8f8643a5a4e18d83a35034f6e
Gytube <fdelattre12@gmail.com>
2026-05-23 23:48:06 +0200

    Make air tooltip translatable
    Replace hard-coded English air tooltip in three chestplate classes with translatable keys and wrap result in parentheses for display. Updated files: RefinedRadianceChestplate, ShadowRadianceChestplate, ShadowSteelChestplate now use createqol.tooltip.use_air.powering or .breathing based on config. Added corresponding entries to en_us.json for the two new translation keys.

--- src/main/java/fr/iglee42/createqualityoflife/items/armors/ShadowSteelChestplate.java
+++ src/main/java/fr/iglee42/createqualityoflife/items/armors/ShadowSteelChestplate.java
@@ -60,7 +60,7 @@ public class ShadowSteelChestplate extends BacktankItem.Layered implements QOLCo
 
     @Override
     public void appendHoverText(ItemStack stack, @Nullable TooltipContext p_41422_, List<Component> components, TooltipFlag p_41424_) {
-        String airTooltip = CreateQOLConfigs.server().equipments.useAir.get() ? " (Powering equipment with air)" : " (Underwater breathing only)";
+        String airTooltip = " (" + Component.translatable(CreateQOLConfigs.server().equipments.useAir.get() ? "createqol.tooltip.use_air.powering" : "createqol.tooltip.use_air.breathing").getString() + ")";
         if (!stack.getOrDefault(QOLDataComponents.ITEM_TOOLTIPS, ItemTooltips.DEFAULT).isEnable(ItemTooltips.Tooltip.OPTIONS)) return;
         components.add(Component.translatable("createqol.ability.armor.toggle_message", Component.translatable("createqol.ability.armor.air").getString())
                 .withStyle(ChatFormatting.GOLD)
